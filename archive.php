389823fa0fd2b6a986e7b6b192b9c1b4a8a945cf
gundyn <nathan.gundy@gmail.com>
2021-04-13 11:39:43 -0500

    boot test

--- archive.php
+++ archive.php
@@ -9,40 +9,41 @@
 
 get_header(); ?>
 
-	<section id="primary" class="container content-area col-sm-12 col-lg-8">
-		<div id="main" class="row site-main" role="main">
+	<section id="primary" class="content-area col-sm-12 col-lg-8">
+		<div id="main" class="site-main" role="main">
+			<div class="container">
 
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				if ( have_posts() ) : ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					<header class="page-header">
+						<?php
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_description( '<div class="archive-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
 
-			endwhile;
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
 
-			the_posts_navigation();
+					endwhile;
 
-		else :
+					the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+				else :
 
-		endif; ?>
+					get_template_part( 'template-parts/content', 'none' );
 
+				endif; ?>
+			</div>
 		</div><!-- #main -->
 	</section><!-- #primary -->
 
--- page.php
+++ page.php
@@ -14,22 +14,25 @@
 
 get_header(); ?>
 
-	<section id="primary" class="container content-area col-sm-12 col-lg-8">
-		<div id="main" class="row site-main" role="main">
+	<section id="primary" class="content-area col-sm-12 col-lg-8">
+		<div id="main" class="site-main" role="main">
+				<div class="container">
+					<div class="row">
 
-			<?php
-			while ( have_posts() ) : the_post();
+					<?php
+					while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+						get_template_part( 'template-parts/content', 'page' );
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
+		                // If comments are open or we have at least one comment, load up the comment template.
+		                if ( comments_open() || get_comments_number() ) :
+		                    comments_template();
+		                endif;
 
-			endwhile; // End of the loop.
-			?>
-
+					endwhile; // End of the loop.
+					?>
+				</div>
+			</div>
 		</div><!-- #main -->
 	</section><!-- #primary -->
 
